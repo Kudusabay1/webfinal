89439ac5e5839e2ee3c861e690b19fb5651de47c
Kudusabay1 <147732724+Kudusabay1@users.noreply.github.com>
2024-05-30 09:21:14 -0500

    addcontent5-30

--- program.js
+++ program.js
@@ -84,6 +84,7 @@ function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     var total = 0
+    var tax = 0
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
@@ -91,7 +92,9 @@ function updateCartTotal() {
         var price = parseFloat(priceElement.innerText.replace('$', ''))
         var quantity = quantityElement.value
         total = total + (price * quantity)
+
     }
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+    document.getElementsByClassName('cart-tax-price')[0].innerText = '$' + tax
 }
